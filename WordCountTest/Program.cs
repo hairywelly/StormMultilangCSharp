2af1fc5d2e0a610513b0897a4f9ffdcbb5792afb
Gareth Smith <gjs585@hotmail.com>
2014-06-20 15:40:44 +0100

    typo

--- WordCountTest/Program.cs
+++ WordCountTest/Program.cs
@@ -6,7 +6,7 @@ namespace WordCountTest
     {
         static void Main(string[] args)
         {
-            var config = new StrormConfigurationBuilder().DontBotherWithTaksIds();
+            var config = new StrormConfigurationBuilder().DontBotherWithTaskIds();
             var bolt = new SplitSentence(config.Reader(), config.BoltWriter());
             bolt.Run();
         }
