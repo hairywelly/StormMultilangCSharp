25c2293351ff6a0823f981fe29ecd5f0b4162f01
Tony Abell <tony@tonyabell.com>
2014-10-08 15:27:13 -0700

    removed kernel32.dll reference, update Split Sentence Bold, to ShellBolt

--- JavaWordCountTest/src/main/java/com/hairywelly/storm/SplitSentence.java
+++ JavaWordCountTest/src/main/java/com/hairywelly/storm/SplitSentence.java
@@ -1,15 +1,16 @@
 package com.hairywelly.storm;
 
+import backtype.storm.task.ShellBolt;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 
 import java.util.Map;
 
-public class SplitSentence extends ShellBoltForNonScripts implements IRichBolt {
+public class SplitSentence extends ShellBolt implements IRichBolt {
 
     public SplitSentence() {
-        super("WordCountTest.exe");
+        super("mono","WordCountTest.exe");
     }
 
     @Override
